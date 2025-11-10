3e295aab44e548116eda286f2f19185910e0f2a9
Your Name <riccardo.buso.2@studenti.unipd.it>
2025-11-10 22:30:15 +0100

    primo

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>        
+<div class="header-text">
+            <p style="text-align: center; width: 100%; margin: 0 auto;">
+<!--NOME  -->
+            </p>
+        </div>ml <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <header class="header">
+        <nav class="main-navigation">
+        </nav>
+        <h1><!--NOME--></h1>
+
+        <div class="header-text">
+            <p style="text-align: center; width: 100%; margin: 0 auto;">
+<!--MOTTO -->
+            </p>
+        </div>
+    </header>
+
